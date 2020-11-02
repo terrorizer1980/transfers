4c4ed8d45c3cdda8514109df77ee9c30d04d6a2b
LayneHaber <layne.haber@gmail.com>
2020-11-01 21:40:03 -0700

    tests added

---
+++ contracts/interfaces/ITransferDefinition.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental "ABIEncoderV2";
+
+struct Balance {
+    uint256[2] amount; // [alice, bob] in channel, [initiator, responder] in transfer
+    address payable[2] to; // [alice, bob] in channel, [initiator, responder] in transfer
+}
+
+struct RegisteredTransfer {
+    string name;
+    address definition;
+    string stateEncoding;
+    string resolverEncoding;
+}
+
+interface ITransferDefinition {
+    // Validates the initial state of the transfer.
+    // Called by validator.ts during `create` updates.
+    function create(bytes calldata encodedBalance, bytes calldata)
+        external
+        view
+        returns (bool);
+
+    // Performs a state transition to resolve a transfer and returns final balances.
+    // Called by validator.ts during `resolve` updates.
+    function resolve(
+        bytes calldata encodedBalance,
+        bytes calldata,
+        bytes calldata
+    ) external view returns (Balance memory);
+
+    // Should also have the following properties
+    // string name
+    // string stateEncoding
+    // string resolverEncoding
+    // These properties are included on the transfer specifically
+    // to make it easier for implementers to add new transfers by
+    // only include a `.sol` file
+    function getRegistryInformation()
+        external
+        view
+        returns (RegisteredTransfer memory);
+}
